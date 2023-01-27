43126f4d3246e4c3f4b87cdd477f78ab3f662110
Miguel S. Silva <miguelprogs.silva@gmail.com>
2023-01-27 01:56:16 -0300

    1° aula

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { Loading } from '@components/Loading';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { SiginUp } from '@screens/SingUp';
+import { NativeBaseProvider } from 'native-base';
+import { StatusBar, View } from 'react-native';
+
+import { THEME } from './src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SiginUp /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
+
+
+//
