74905fca0e6308237b590d146675db536ade9e73
Miguel S. Silva <miguelprogs.silva@gmail.com>
2023-03-07 02:35:29 -0300

    Formularios e validações

--- src/screens/public/SignUp.tsx
+++ src/screens/public/SignUp.tsx
@@ -4,12 +4,28 @@ import LogoSvg from '@assets/logo.svg';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 import { Center, Heading, Image, Text, VStack, ScrollView } from 'native-base';
+import { useForm, Controller } from 'react-hook-form'
+
+
+type FormDataProps = {
+    name: string;
+    email: string;
+    password: string;
+    password_confirm: string;
+}
 
 export function SignUp() {
+
+    const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
+
     const navigation = useNavigation();
 
     function handleGoBack() {
         navigation.goBack()
+    }
+
+    function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+        console.log({ name, email, password, password_confirm })
     }
 
     return (
@@ -38,28 +54,79 @@ export function SignUp() {
                         Crie sua conta
                     </Heading>
 
-                    <Input
-                        placeholder='Nome'
+                    <Controller
+                        name="name"
+                        control={control}
+                        rules={{ required: 'Informe o nome' }}
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder='Nome'
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
 
-                    <Input
-                        placeholder='E-mail'
-                        keyboardType='email-address'
-                        autoCapitalize='none'
+                    <Text color="white">
+                        {errors.name?.message}
+                    </Text>
+
+                    <Controller
+                        name="email"
+                        control={control}
+                        rules={{
+                            required: 'Informe o E-mail',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'E-mail inválido'
+                            }
+                        }}
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder='E-mail'
+                                keyboardType='email-address'
+                                autoCapitalize='none'
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
 
-                    <Input
-                        placeholder='Senha'
-                        secureTextEntry
+                    <Text color="white">
+                        {errors.email?.message}
+                    </Text>
+
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder='Senha'
+                                secureTextEntry
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
                     />
 
-                    <Input
-                        placeholder='Confirmar a senha'
-                        secureTextEntry
+                    <Controller
+                        control={control}
+                        name="password_confirm"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                placeholder='Confirmar a senha'
+                                secureTextEntry
+                                onChangeText={onChange}
+                                onSubmitEditing={handleSubmit(handleSignUp)}
+                                returnKeyType="send"
+                                value={value}
+                            />
+                        )}
                     />
 
                     <Button
                         title='Criar e acessar'
+                        onPress={handleSubmit(handleSignUp)}
                     />
                 </Center>
 
