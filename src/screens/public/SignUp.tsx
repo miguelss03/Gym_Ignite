ef0197661449bf7d5d4a1e45955a163f6fafbe2f
miguelss03 <miguelprogs.silva@gmail.com>
2023-04-02 03:21:36 -0300

    CADASTRANDO DADOS NA API

--- src/screens/public/SignUp.tsx
+++ src/screens/public/SignUp.tsx
@@ -25,13 +25,22 @@ const signUpSchema = yup.object({
 
 export function SignUp() {
     const navigation = useNavigation();
-    
+
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
     });
 
-    function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-        console.log({ name, email, password, password_confirm })
+    async function handleSignUp({ name, email, password }: FormDataProps) {
+        const response = await fetch('http://192.168.1.4:3333/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        })
+        const data = await response.json();
+        console.log(data);
     }
 
     function handleGoBack() {
